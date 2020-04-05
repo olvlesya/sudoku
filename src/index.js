8cd933fbdb711458cf108ae62def2bd26990dc4c
Olesia Nikonova <lesyonoknou@gmail.com>
2020-04-05 17:16:01 +0200

    feat: implement simple solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  for (let row = 0; row < 9; row++) {
+    for (let column = 0; column < 9; column++) {
+      if (matrix[row][column] === 0) {
+        const rowMissingNumbers = getMissingNumbers(matrix[row]);
+        if (rowMissingNumbers.length === 1) {
+          matrix[row][column] = rowMissingNumbers[0];
+        } else {
+          const columnArray = getColumnArray(matrix, column);
+          const columnMissingNumbers = getMissingNumbers(columnArray);
+          matrix[row][column] = columnMissingNumbers[0];
+        }
+      }
+    }
+  }
+  return matrix;
+};
+
+function getColumnArray(matrix, column) {
+  const columnArray = [];
+  for (let row = 0; row < 9; row++) {
+    if (matrix[row][column] !== 0) {
+      columnArray.push(matrix[row][column]);
+    }
+  }
+  return columnArray;
+}
+
+function getMissingNumbers(line) {
+  const possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const missingNumbers = [];
+  for (let num of possibleNumbers) {
+    if (line.indexOf(num) === -1) {
+      missingNumbers.push(num);
+    }
+  }
+  return missingNumbers;
 }
